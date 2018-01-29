b79177178de508a3fbc953cf1e239da6e9bc8664
GavinG <GavinG@nvisionit.co.za>
2018-01-29 07:50:03 +0200

    Fixed Issue #25 Fix [Del] key with a selection

--- src/input.handler.ts
+++ src/input.handler.ts
@@ -76,7 +76,10 @@ export class InputHandler {
                 this.onModelChange(this.inputService.value);
             }
 
-            if (keyCode == 8 && selectionRangeLength != 0 && !isNaN(this.inputService.value)) {
+            if (
+                (keyCode === 8 || keyCode === 46)
+                && selectionRangeLength != 0 && !isNaN(this.inputService.value)
+            ) {
                 this.inputService.removeNumber(keyCode);
                 this.onModelChange(this.inputService.value);
             }
