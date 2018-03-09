2de49f2f55e0f1032de23cb875ccd5c6595e2d26
César Mendonça <cesar.rew@gmail.com>
2018-03-09 13:06:39 -0300

    Allows backspace or delete with a selection.

--- src/input.service.ts
+++ src/input.service.ts
@@ -86,8 +86,7 @@ export class InputService {
         }
 
         selectionEnd = keyCode == 46 || keyCode == 63272 ? selectionEnd + 1 : selectionEnd;
-        let lengthOfZero = selectionEnd === selectionStart ? true : false;
-        selectionStart = keyCode === 8 && lengthOfZero ? selectionStart - 1 : selectionStart;
+        selectionStart = keyCode == 8 && (selectionEnd == selectionStart) ? selectionStart - 1 : selectionStart;
         this.rawValue = this.rawValue.substring(0, selectionStart) + this.rawValue.substring(selectionEnd, this.rawValue.length);
         this.updateFieldValue(selectionStart);
     }
