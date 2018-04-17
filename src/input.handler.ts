c87d387b7d854eca7336e0407463e7f3ac0d8afb
EmaGht <ema.2@hotmail.it>
2018-04-17 10:48:27 +0200

    Fix for Firefox copy/paste/cut browsers
    When you press CTRL+C, CTRL+V on firefox, for some reason it doesn't send "paste" or "copy" events, but a "keyup" event with "v" or "c" keyCode with a ctrlKey modifier.
    
    With this fix everything gets handled how it should

--- src/input.handler.ts
+++ src/input.handler.ts
@@ -112,6 +112,23 @@ export class InputHandler {
             return;
         }
 
+        //Copy/Cut, call the right methods
+		if(event.charCode == 118 && event.ctrlKey)
+		{
+			this.handlePaste(event);
+			return;
+		}
+		
+		if(event.charCode == 120 && event.ctrlKey)
+		{
+			this.handleCut(event);
+			return;
+		}
+		
+		//Paste. Nothing to do here
+		if(event.charCode == 99 && event.ctrlKey)
+			return;
+
         let keyCode = event.which || event.charCode || event.keyCode;
 
         if (keyCode == undefined || [9, 13].indexOf(keyCode) != -1 || this.isArrowEndHomeKeyInFirefox(event)) {
