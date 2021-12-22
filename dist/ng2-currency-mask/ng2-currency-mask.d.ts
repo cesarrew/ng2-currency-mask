6eb30ca130525edb3a45df3e8900515ed6f4f11b
César Mendonça <cesar.rew@gmail.com>
2021-12-22 11:02:47 -0300

    Fixing configuration issues.

--- dist/ng2-currency-mask/ng2-currency-mask.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="ng2-currency-mask" />
-export * from './public-api';
--- dist/ng2-currency-mask/public-api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./lib/currency-mask.config";
-export * from "./lib/currency-mask.directive";
-export * from "./lib/currency-mask.module";
