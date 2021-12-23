a573368e4c6601856747c3a60c49af25ee1a3d2c
César Mendonça <cesar.rew@gmail.com>
2021-12-23 08:57:01 -0300

    Improving the library-test project.

--- projects/library-test/src/app/app.component.ts
+++ projects/library-test/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styles: [],
 })
-export class AppComponent {
-    reactiveFormControl = new FormControl(10);
-    templateDrivenValue: number = 20;
+export class AppComponent implements OnInit {
+    formControlExample = new FormControl(20);
+    formGroupExample: FormGroup;
+    ngModelExample: number = 10;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.formGroupExample = this.formBuilder.group({
+            formArrayExample: this.formBuilder.array([this.formBuilder.control(30), this.formBuilder.control(40), this.formBuilder.control(50)]),
+        });
+    }
+
+    get formArrayExample() {
+        return this.formGroupExample.get("formArrayExample") as FormArray;
+    }
 }
